9ac41c1d4de09aea33a4012d127fd272a1da05a2
Chad Woolley <thewoolleyman@gmail.com>
2017-05-05 18:39:23 -0700

    ch 13 - logging

--- exercises/chapter-13/01-OrganizingAProject-1/issues/lib/issues/github_issues.ex
+++ exercises/chapter-13/01-OrganizingAProject-1/issues/lib/issues/github_issues.ex
@@ -3,10 +3,13 @@ defmodule Issues.GithubIssues do
     Get github issues
   """
 
+  require Logger
+
   @github_url Application.get_env(:issues, :github_url)
   @user_agent [{"User-agent", "Elixir thewoolleyman@gmail.com"}]
 
   def fetch(user, project) do
+    Logger.info "Fetching user #{user}'s project #{project}"
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_response
@@ -17,10 +20,13 @@ defmodule Issues.GithubIssues do
   end
   
   def handle_response({:ok, %{status_code: 200, body: body}}) do
+    Logger.info "Successful response"
+    Logger.debug fn -> inspect(body) end
     {:ok, Poison.Parser.parse!(body)}
   end
   
-  def handle_response({_, %{status_code: _, body: body}}) do
+  def handle_response({_, %{status_code: status, body: body}}) do
+    Logger.error "Error #{status} returned"
     {:error, Poison.Parser.parse!(body)}
   end
 end
