ea9111674311a25b9282443a895419406b8ba608
Chad Woolley <thewoolleyman@gmail.com>
2017-05-07 20:47:29 -0700

    ch 15, working with processes 3..5

---
+++ exercises/chapter-15/05a-WorkingWithMultipleProcesses-5a.exs
@@ -0,0 +1,27 @@
+defmodule Waiter do
+  import :timer, only: [ sleep: 1]
+
+  def reply(sender) do
+    send(sender, :reply)
+    exit(:success)
+  end
+
+  def get_messages do
+    receive do
+      reply ->
+        IO.inspect reply
+        get_messages()
+    after 500 ->
+      IO.puts "No more messages"
+    end
+  end
+
+  def run do
+    Process.flag(:trap_exit, true)
+    spawn_monitor(Waiter, :reply, [self()])
+    sleep 100
+    get_messages()
+  end
+end
+
+Waiter.run
