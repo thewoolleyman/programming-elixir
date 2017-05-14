b8d3376f377eefc8514d7e26fdb88a683cdf594e
Chad Woolley <thewoolleyman@gmail.com>
2017-05-13 20:38:39 -0700

    ch17 ex5

--- exercises/chapter-17/01-OTP-Servers-1/stack/lib/stack/server.ex
+++ exercises/chapter-17/01-OTP-Servers-1/stack/lib/stack/server.ex
@@ -15,6 +15,10 @@ defmodule Stack.Server do
     GenServer.cast(__MODULE__, {:push, value})
   end
 
+  def stop(reason) do
+    GenServer.cast(__MODULE__, {:stop, reason})
+  end
+
   # GenServer implementation
 
   def handle_call(:pop, _from, stack) do
@@ -23,6 +27,17 @@ defmodule Stack.Server do
   end
 
   def handle_cast({:push, value}, stack) do
-    {:noreply, [value | stack ]}
+    cond do
+      value < 10 -> {:stop, :value_must_be_greater_than_ten, stack}
+      true -> {:noreply, [value | stack ]}
+    end
+  end
+
+  def handle_cast({:stop, reason}, stack) do
+    {:stop, reason, stack}
+  end
+
+  def terminate(reason, stack) do
+    IO.puts("Terminating.  Reason: #{reason}, Stack: #{inspect stack}")
   end
 end
