cb6a2b3476c0bac264342145bd349a82ea9c1b6b
Chad Woolley <thewoolleyman@gmail.com>
2017-05-13 20:22:09 -0700

    ch17 ex4

--- exercises/chapter-17/01-OTP-Servers-1/stack/lib/stack/server.ex
+++ exercises/chapter-17/01-OTP-Servers-1/stack/lib/stack/server.ex
@@ -1,6 +1,22 @@
 defmodule Stack.Server do
   use GenServer
 
+  # External API
+
+  def start_link(initial_stack) do
+    GenServer.start_link(__MODULE__, initial_stack, name: __MODULE__)
+  end
+
+  def pop do
+    GenServer.call(__MODULE__, :pop)
+  end
+
+  def push(value) do
+    GenServer.cast(__MODULE__, {:push, value})
+  end
+
+  # GenServer implementation
+
   def handle_call(:pop, _from, stack) do
     [head | tail] = stack
     {:reply, head, tail}
